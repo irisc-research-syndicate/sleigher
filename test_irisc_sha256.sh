abd78b20b9f5fa056a30aecb181fdcc144a7fdd5
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2024-10-21 10:02:38 +0200

    fix test_irisc_sha256.sh

--- test_irisc_sha256.sh
+++ test_irisc_sha256.sh
@@ -6,7 +6,7 @@ cargo run --bin sleigh-emu -- \
     --map 0x00710000:00007000_0001c494_IRON_PREP_CODE \
     --ram 0xffff0000:0x10000 \
     --entrypoint 0x007230cc \
-    -r r4=0x00710000 -r r5=0x37 -r r1=0xfffff000 -r r6=0xffffff80 \
+    -r r1=0xfffff000 -r r4=0x00710000 -r r5=0x37 -r r6=0xffffff80 \
     -b 0x007230cc:sha256:r1l,r4l,r5l,r6l \
     -b 0x00722d8c:sha256_transform:r1l,r4l \
     -b 0x00722cf8:sha256_update:r1l,r4l,r5l,r6l \
