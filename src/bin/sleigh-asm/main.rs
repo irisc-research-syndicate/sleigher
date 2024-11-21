cb8d06b1eb5fdcc087c642f02760b07791c891c7
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2024-11-21 11:53:29 +0100

    write instruction bytes

--- src/bin/sleigh-asm/main.rs
+++ src/bin/sleigh-asm/main.rs
@@ -451,17 +451,21 @@ impl Assembler {
 }
 
 pub fn main() -> Result<()> {
+    env_logger::init();
+
     let args = Cli::parse();
     let sleigh = sleigh_rs::file_to_sleigh(&args.slaspec.clone()).ok().context("Could not open or parse slaspec")?;
     let assembler = Assembler::new(sleigh);
     let (rest, constraints) = assembler.assemble_instruction(&args.instruction).ok().context("Failed to parse instruction")?;
 
     println!("rest: {:?}", rest);
+
     println!("token_order: {:?}", constraints.token_order);
     println!("tokens: {:?}", constraints.tokens);
     println!("fields: {:#?}", constraints.fields.values());
     println!("eqs: {:#?}", constraints.eqs);
     println!("model: {:#?}", constraints.model());
+    println!("bytes: {:02x?}", constraints.to_bytes());
 
     Ok(())
 }
